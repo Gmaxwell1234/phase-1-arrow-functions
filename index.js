e64c527e8de8854a22247ef4013aaf9fd4988e34
Maxwell Githinji <muriumaxwell1@gmail.com>
2025-03-06 14:21:04 +0300

    creates index,js

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide= (a, b)=>{
+    let result = a/b;
+    return result;
+}
+console.log (divide(2000, 100));
+
+const square= num=> num * num;
+const add= (a, b)=>{
+    let result= a + b;
+    return result;
+}
+console.log(add(3,4));
